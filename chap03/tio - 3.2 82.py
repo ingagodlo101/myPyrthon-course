3ffd91f117353bc38d7296c3f2d3e554458a95e7
ingagodlo101 <ingagodlo>
2020-07-06 15:03:15 +0200

    chap 4 done

---
+++ chap03/tio - 3.2 82.py
@@ -0,0 +1,6 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+
+    print(f"{magician.title()}, that was a great trick!")
+
+print("Thank you, everyone. That was a great magic show!")
---
+++ chap04/tiy-4.1 105.py
@@ -0,0 +1,6 @@
+pizzas = ['something meaty', 'chicken and mushroom', 'Hot one']
+for pizza in pizzas:
+    print(f"{pizza.title()}, Very nice to eat for any meal!")
+print("\n")
+print(f"My favourite of them all is , {pizzas[1].title()}.\n")
+print('I really love pizza')
